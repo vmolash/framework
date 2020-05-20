749f4f2548f26d8b992283ba89dcb417d5053c38
Volha Molash <48703723+vmolash@users.noreply.github.com>
2020-05-19 23:48:44 -0700

    Mobile nav (#2)
    * search box, template for docs.html
    
    * Mobile navigation

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -13,4 +13,32 @@ function myFunction() {
             li[i].style.display = "none";
         }
     }
+
 }
+
+// Active link sets to green color when click on it
+// const NAVCONTAINER = document.querySelector('nav');
+// const NAVITEMS = NAVCONTAINER.getElementsByClassName('nav-item');
+
+// for(let i = 0; i < NAVITEMS.length; i++) {
+//   NAVITEMS[i].addEventListener('click', function() {
+//     let current = document.getElementsByClassName('active');
+//     current[0].className = current[0].className.replace(' active', '');
+//     this.className += ' active';
+//   });
+// }
+
+// Hide navigation on mobile version when click on any link
+// console.log(window.screen.width);
+// const NAVIGAT = document.querySelector('nav');
+// const LAB = document.querySelector('label');
+
+// if(window.screen.width < 500) {
+//   LAB.addEventListener('click', function() {
+//     NAVIGAT.classList.toggle('show');
+//   });
+//   NAVIGAT.addEventListener('click', function() {
+//     this.classList.toggle('show');
+//   });
+// }
+

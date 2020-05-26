f34159e4a05badbf2ebdad0cabcace9206d59a0e
vmolash <molash.v@gmail.com>
2020-05-25 20:03:43 -0700

    buttons, colors, fonts, font-decoration

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -63,7 +63,7 @@ function searchDesktop() {
 
 
 
-// Accordion
+// ACCORDION
 var acc = document.getElementsByClassName("accordion");
 var i;
 
@@ -82,6 +82,157 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// CUSTOM FORM
+var x, i, j, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < selElmnt.length; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < s.length; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            for (k = 0; k < y.length; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
+
+
+// SLIDESHOW
+    var slides = document.querySelectorAll('#slides .slide');
+    console.log(slides);
+    var currentSlide = 0;
+    var slideTimer = setInterval(nextSlide, 2000);
+
+    function autoSlide(n) {
+      slides[currentSlide].className = 'slide';
+      currentSlide = (n+slides.length)%slides.length; // 1%6 = 1, 2%6 = 2 ... 6%6=0
+      console.log(currentSlide);
+      slides[currentSlide].className = 'slide showing';
+    }
+    // pause button
+    var playing = true;
+    var pauseBtn = document.getElementById('pause');
+
+    function pauseSS() {
+        
+        pauseBtn.innerHTML = '<i class="fas fa-play-circle"></i>';
+        
+        playing = false;
+        clearInterval(slideTimer);
+        console.log(playing,'it is paused');
+    } // play button
+    function playSS() {
+       
+        pauseBtn.innerHTML = '<i class="fas fa-pause-circle"></i>';
+        playing = true;
+        slideTimer = setInterval(nextSlide, 2000);
+        console.log(playing, 'it is playing');
+    }
+    // add even to pause/play
+    pauseBtn.addEventListener('click', function(){
+        console.log('pauseBtn');
+        if(playing) {
+            pauseSS();
+        } else {
+            playSS();
+        } 
+        });
+    // next/prev slides
+    function nextSlide () {
+        // increase count of slide array by 1
+        autoSlide(currentSlide+1);
+        
+        
+    }
+    function prevSlide() {
+        // decrease count of slide array by 1
+       
+       autoSlide(currentSlide-1);
+       
+    }
+ // reference next and prev buttons
+    var nextBtn = document.getElementById('next');
+    var prevBtn = document.getElementById('prev');
+
+    // add event listener to listen for click
+    nextBtn.addEventListener('click', function(){
+        pauseSS();
+        nextSlide();
+    });
+    prevBtn.addEventListener('click', function(){
+        pauseSS();
+        prevSlide();
+    } );
+
+// END SLIDESHOW
+
+
+
+
+
 
 // const mainSections = document.querySelectorAll('section h2');
 //     const appearSections = {
